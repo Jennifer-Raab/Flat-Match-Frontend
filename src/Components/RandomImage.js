a5b4a1143b1e8d9d35107e794c7569f30034ee06
Roland Kirchhoff <rolandkirchhoff@web.de>
2022-10-18 21:04:56 +0200

    zufallsbild

--- src/Components/RandomImage.js
+++ src/Components/RandomImage.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { useLocation } from 'react-router-dom';
 import image01 from "../images/bild01.jpg";
 import image02 from "../images/bild02.jpg";
 import image03 from "../images/bild04.jpg";
@@ -17,20 +18,24 @@ const images = [
     image07,
 ];
 
+
 export default function RandomImage() {
     
     const [randomImage, setRandomImage] = useState();
+    const location = useLocation();
+    console.log(location.pathname);
 
     useEffect(() => {
         let randnum = Math.floor(Math.random() * 7);
         console.log("Random", randnum);
         setRandomImage(images[randnum]);
-    }, []);
-
-    return (
-      <div className="randomimage">
-        <img src={randomImage} alt="" />
-      </div>
-    );
+    }, [location.pathname]);
+    if ((location.pathname.indexOf("gesuch/") === -1) || (location.pathname.indexOf("angebot/") === -1)) {
+      return (
+        <div className="randomimage">
+          <img src={randomImage} alt="" />
+        </div>
+      );
+    }
   }
   
