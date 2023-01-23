87351f70ca02b148e702a7a99e4fbf618b59fca7
Roland Kirchhoff <rolandkirchhoff@web.de>
2023-01-23 15:59:27 +0100

    Bildupload

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 const firebaseConfig = {
-  apiKey: "AIzaSyA-zePd6BLSb6vAiVGQjYI996x1Akq-Ul0",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "flat-match-b51cb.firebaseapp.com",
   projectId: "flat-match-b51cb",
   storageBucket: "flat-match-b51cb.appspot.com",
-  messagingSenderId: "353244370768",
-  appId: "1:353244370768:web:2b3f9a0f36f18e997cbe55",
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
